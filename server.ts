19338d5b9d904347b03e9ce82ac02e8bc79b9256
Nabil Habrani <s123491@ap.be>
2026-04-23 09:48:53 +0000

    .

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import ejs from "ejs";
+
+
+const app = express();
+
+app.set("view engine", "ejs"); // EJS als view engine
+app.set("port", 3000);
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+
+app.listen(app.get("port"), () =>
+  console.log("[server] http://localhost:" + app.get("port"))
+);
